70da7c4f40013413bd92b1f1eaf95ff0562aeeec
Adriaan Pelzer <adriaan.pelzer@trinitymirror.com>
2017-07-06 16:44:24 +0000

    Deconstruct-api first commit

---
+++ deconstruct.js
@@ -0,0 +1,91 @@
+const H = require ( 'highland' );
+const R = require ( 'ramda' );
+const express = require ( 'express' ), app = express ();
+const bodyParser = require ( 'body-parser' );
+const fs = require ( 'fs' ), dirStream = H.wrapCallback ( R.bind ( fs.readdir, fs ) );
+const itemStore = require ( '../../lib/itemStore.js' );
+
+const utils = {
+    getClients: itemStore.getClients,
+    log: R.compose ( console.log, R.partialRight ( JSON.stringify, [ null, 4 ] ) ),
+    streamRoute: H.wrapCallback ( ( routeName, utils, req, res, callback ) => {
+        return require ( `./routes/${routeName}` )( R.assocPath ( [ 'callback' ], ( res, error, result ) => {
+            if ( error === undefined && result === undefined ) {
+                return callback;
+            }
+
+            return callback ( error, result );
+        }, utils ), req, res );
+    } ),
+    error: ( res, error ) => {
+        const localCallback = error => {
+            return res.status ( error.code && parseInt ( error.code, 10 ) >= 200 ? parseInt ( error.code, 10 ) : 500 ).json ( error );
+        };
+
+        if ( error === undefined ) {
+            return localCallback;
+        }
+
+        return localCallback ( error );
+    },
+    callback: ( res, error, result ) => {
+        const localCallback = ( error, result ) => {
+            if ( error ) {
+                return utils.error ( res, error );
+            }
+
+            return res.json ( result );
+        };
+
+        if ( res === undefined ) {
+            throw new Error ( 'utils.callback needs the response object as its first argument' );
+        }
+
+        if ( error === undefined && result === undefined ) {
+            return localCallback;
+        }
+
+        return localCallback ( error, result );
+    }
+};
+
+app.use ( bodyParser.json () );
+
+app.use ( ( req, res, next ) => {
+    res.header ( 'Access-Control-Allow-Origin', '*' );
+    res.header ( 'Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS' );
+    res.header ( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept' );
+    next();
+} );
+
+module.exports = {
+    loadRoutes: ( routeDir, callback ) => {
+        return dirStream ( routeDir )
+            .sequence ()
+            .filter ( route => R.reduce ( ( accept, re ) => {
+                return accept && R.last ( route.split ( '/' ) ).match ( re );
+            }, true, [
+                /^~/,
+                /\.js$/
+            ] ) )
+            .doto ( route => {
+                const routeComponents = route.replace ( /\.js$/, '' ).split ( '~' );
+                const pathSpec = R.init ( routeComponents ).join ( '/' );
+                const method = R.last ( routeComponents );
+
+                console.log ( `registering ${pathSpec} ${method}` );
+
+                app[method] ( pathSpec, require ( `${routeDir}/${route}` )( utils ) );
+            } )
+            .collect ()
+            .toCallback ( callback );
+    },
+    start: port => {
+        app.get ( '/healthcheck', ( req, res ) => res.json ( 200 ) );
+        app.listen ( port );
+    },
+    getApp: () => {
+        return app;
+    }
+};
+
